7e9afca13d1d3c068ae11b9e233216bf5c8fb252
Sigve Hansen <sighan12@gmail.com>
2022-10-04 19:51:05 +0200

    fix(login): use enhance

--- src/routes/(app)/login/+page.svelte
+++ src/routes/(app)/login/+page.svelte
@@ -1,10 +1,18 @@
 <script lang="ts">
+	import { fly } from 'svelte/transition'
+	import { enhance } from '$app/forms'
+
 	import Button from '$lib/components/Button.svelte'
 	import Heading from '$lib/components/headings/Heading.svelte'
 	import Input from '$lib/components/Input.svelte'
 </script>
 
-<form action="" class="mx-auto flex max-w-md flex-col gap-6">
+<form
+	action="?/login"
+	use:enhance
+	class="mx-auto flex w-full max-w-md flex-col gap-6"
+	in:fly={{ y: 10, duration: 400 }}
+>
 	<Heading class="text-4xl font-bold lg:text-5xl">Log in</Heading>
 	<Input label="Email" />
 	<Input label="Password" type="password" />
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,7 +2,6 @@
 	import '../app.css'
 	import Container from '$lib/components/Container.svelte'
 	import HeadingLevel from '$lib/components/headings/HeadingLevel.svelte'
-
 	import Header from '$lib/components/Header.svelte'
 	import Footer from '$lib/components/Footer.svelte'
 </script>
