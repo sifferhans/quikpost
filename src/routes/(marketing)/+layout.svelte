7c0484f48bbabfc022fbc394766f81677dafdfeb
Sigve Hansen <sighan12@gmail.com>
2022-12-16 09:18:11 +0100

    fix(marketing): prerender marketing pages

--- src/routes/(marketing)/+layout.svelte
+++ src/routes/(marketing)/+layout.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
 	import Header from '$lib/components/Header.svelte'
 	import Footer from '$lib/components/Footer.svelte'
+
+	export const prerender = true
 </script>
 
 <Header slot="header" />
