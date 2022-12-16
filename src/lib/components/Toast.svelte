f1cae580b521d7d41d9851ada475a1a98a761c7f
Sigve Hansen <sighan12@gmail.com>
2022-12-16 09:18:33 +0100

    [WIP]: start toast component

--- src/lib/components/Toast.svelte
+++ src/lib/components/Toast.svelte
@@ -1,23 +1,23 @@
 <script lang="ts">
-	// TODO: make toast component
+	import { AlertCircle, AlertTriangle, InfoCircle } from 'tabler-icons-svelte'
 	export let type: 'success' | 'error' | 'info' = 'info'
 	export let message: string
 </script>
 
 <div
-	class="fixed bottom-0 right-0 z-50 mx-auto mb-4 w-full max-w-sm overflow-hidden rounded-lg border border-gray-900 bg-white shadow-md"
+	class="fixed top-6 right-6 z-50 mx-auto mb-4 w-full max-w-sm overflow-hidden rounded-lg border border-gray-900 bg-white shadow-md"
 >
 	<div class="p-4">
 		<div class="flex items-center justify-between">
 			<span
-				class="text-sm {type === 'success'
+				class="flex gap-2 text-sm {type === 'success'
 					? 'text-green-600'
 					: type === 'error'
 					? 'text-red-600'
 					: ''}"
 			>
 				{#if type === 'success'}
-					Success
+					<InfoCircle size="20" /> Success
 				{:else if type === 'error'}
 					Error
 				{:else}
@@ -28,7 +28,7 @@
 				class="transform rounded-md p-1 transition-colors duration-200 hover:bg-gray-100 focus:bg-gray-200 focus:outline-none"
 			>
 				<svg
-					class="h-5 w-5"
+					class="h-4 w-4"
 					viewBox="0 0 24 24"
 					stroke-width="2"
 					stroke="currentColor"
@@ -42,8 +42,8 @@
 				</svg>
 			</button>
 		</div>
-		<div class="mt-2">
-			<p class="text-sm text-gray-900">
+		<div>
+			<p class="mt-1 text-sm text-gray-900">
 				{message}
 			</p>
 		</div>
