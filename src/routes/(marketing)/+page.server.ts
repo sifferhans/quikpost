6c52ad609100c1d6892d838d779ab383f21d49e2
Sigve Hansen <sighan12@gmail.com>
2023-03-06 12:11:57 +0100

    build: move to pnpm

--- src/routes/(marketing)/+page.server.ts
+++ src/routes/(marketing)/+page.server.ts
@@ -3,4 +3,6 @@ import { redirect } from "@sveltejs/kit"
 
 export const load: PageServerLoad = async ({ locals }) => {
 	if (locals.user) throw redirect(302, '/dashboard')
+
+	return {}
 }
--- src/routes/(protected)/+layout.server.ts
+++ src/routes/(protected)/+layout.server.ts
@@ -3,4 +3,6 @@ import { redirect } from "@sveltejs/kit"
 
 export const load: LayoutServerLoad = async ({ locals }) => {
 	if (!locals.user) throw redirect(302, '/')
+
+	return {}
 }
